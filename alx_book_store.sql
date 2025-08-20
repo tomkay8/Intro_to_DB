fa7682cbece61ae020b25015afef89e57bc8ecbf
Kayode Thomas <thomaskayode8@gmail.com>
2025-08-21 00:56:00 +0100

    A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,44 +1,36 @@
--- Create Database
-CREATE DATABASE alx_book_store;
-
--- Use Database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- Authors Table
 CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
 
--- Books Table
 CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Customers Table
 CREATE TABLE Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
 
--- Orders Table
 CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_Details Table
 CREATE TABLE Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
